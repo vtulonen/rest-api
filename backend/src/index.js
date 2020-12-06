2b375b434c4e33c39b910e1e34e379a2569582d0
Vili Tulonen <tulonenvili@gmail.com>
2020-12-06 16:31:51 +0200

    Integrate MySQL in Docker dev env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill';
 import Koa from 'koa';
 import Router from 'koa-router';
+import mysql from 'mysql2/promise';
 
 // The port that this server will run on, defaults to 9000
 const port = process.env.PORT || 9000;
@@ -14,13 +15,33 @@ const test = new Router();
 // Define API path
 const apiPath = '/api/v1';
 
-test.get(`${apiPath}/test`, async (ctx) => {
-  // Tell the HTTP response that it contains JSON data encoded in UTF-8
-  ctx.type = 'application/json; charset=utf-8';
+const connectionSettings = {
+  host: 'db',
+  user: 'root',
+  database: 'db_1',
+  password: 'db_rootpass',
+  namedPlaceholders: true,
+};
 
-  // Put a static greeting message to the HTTP response body that will be sent to the browser
-  // when a client navigates to http://hostname/api/v1/test
-  ctx.body = { greeting: 'Hello world waddap!' };
+test.get(`${apiPath}/test`, async (ctx) => {
+  try {
+    const conn = await mysql.createConnection(connectionSettings);
+    const [data] = await conn.execute(`
+        SELECT *
+        FROM test_table
+      `);
+
+    console.log('Data fetched:', data);
+
+    // Tell the HTTP response that it contains JSON data encoded in UTF-8
+    ctx.type = 'application/json; charset=utf-8';
+
+    // Add stuff to response body
+    ctx.body = { greeting: 'Hello world!', data };
+  } catch (error) {
+    console.error('Error occurred:', error);
+    ctx.throw(500, error);
+  }
 });
 
 app.use(test.routes());
